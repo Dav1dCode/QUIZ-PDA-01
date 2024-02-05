6cf414c4ec769f450c00b7a878bbc3f24bb7a898
leticiaSouzaDev <lec667121@gmail.com>
2024-02-05 17:32:33 -0300

    feat: em construção o algoritmo que recebe a quantidade de tentativas feitas pelo usuário

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-import quizData from './data/quiz_data.js'
+import quizData from './data/quiz_data.js';
+
+let tentativas = 0;
+let tentativasCorretas = 0;
 
 function exibirPergunta(perguntaObj) {
     const respostaUsuario = prompt(perguntaObj.questao + '\n' + perguntaObj.opcoes.join('\n'));
-    
+
     if (respostaUsuario === perguntaObj.resposta) {
         alert('Resposta correta!');
+
+        tentativasCorretas += 1;
+        console.log(tentativasCorretas);
     } else {
         alert('Resposta incorreta. A resposta correta é: ' + perguntaObj.resposta);
     }
+
+    tentativas += 1;
+    console.log(tentativas);
 }
 
 quizData.forEach((pergunta) => {
